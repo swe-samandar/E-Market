a7ae7226486d6a0e097d845361847f14300e3718
Samandar Nemataliyev <samandarnemataliyev@gmail.com>
2025-04-06 15:24:55 +0500

    ready to clone for each team member

---
+++ main/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from .views import (
+    IndexView,
+    ShopView,
+    DetailView,
+    ContactView,
+    AboutView,
+    FAQsView,
+)
+
+app_name = 'main'
+
+urlpatterns = [
+    path('', IndexView.as_view(), name='index'),
+    path('shop/', ShopView.as_view(), name='shop'),
+    path('detail/', DetailView.as_view(), name='detail'),
+    path('contact/', ContactView.as_view(), name='contact'),
+    path('about/', AboutView.as_view(), name='about'),
+    path('faqs/', FAQsView.as_view(), name='faqs'),
+]          
--- main/views.py
+++ main/views.py
@@ -1,3 +1,78 @@
 from django.shortcuts import render
+from django.views import View
+from datetime import datetime
+
+def get_today():
+    return datetime.date(datetime.today())
 
 # Create your views here.
+
+class IndexView(View):
+    def get(self, request):
+
+        context = {
+            'title': 'E-Market',
+            'today': get_today()
+        }
+
+        return render(request, 'main/index.html', context=context)
+    
+
+
+class ShopView(View):
+    def get(self, request):
+
+        context = {
+            'title': 'E-Market - Product Listing Page',
+            'today': get_today()
+        }
+        
+        return render(request, 'main/shop.html', context=context)
+    
+
+
+class DetailView(View):
+    def get(self, request):
+
+        context = {
+            'title': 'E-Market - Product Detail Page',
+            'today': get_today()
+        }
+        
+        return render(request, 'main/shop-single.html', context=context)
+    
+
+
+class ContactView(View):
+    def get(self, request):
+
+        context = {
+            'title': 'E-Market - Contact',
+            'today': get_today()
+        }
+        
+        return render(request, 'main/contact.html', context=context)
+    
+
+
+class AboutView(View):
+    def get(self, request):
+
+        context = {
+            'title': 'E-Market - About Page',
+            'today': get_today()
+        }
+        
+        return render(request, 'main/about.html', context=context)
+
+
+
+class FAQsView(View):
+    def get(self, request):
+
+        context = {
+            'title': 'E-Market - FAQs',
+            'today': get_today()
+        }
+        
+        return render(request, 'main/faqs.html', context=context)
