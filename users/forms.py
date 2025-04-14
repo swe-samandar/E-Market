45a6bcff9e3aa6260900abf045f2ea70015ea9f5
Samandar <154081378+swe-samandar@users.noreply.github.com>
2025-04-14 16:42:29 +0500

    Update forms.py

--- users/forms.py
+++ users/forms.py
@@ -12,9 +12,16 @@ class UserRegistrationForm(forms.ModelForm):
         widgets = {
             'password': forms.PasswordInput()
         }
+       
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password and confirm_password and password != confirm_password:
             raise forms.ValidationError("Passwords do not match.")
         return cleaned_data
-    
+
     def save(self, commit=True):
         user = super().save(commit=False)
         user.set_password(self.cleaned_data['password'])
