58b3b92a05c49b9ec77e270dadb0305949900e49
nurxanov19 <shoxjaxonnurxonov14@gmail.com>
2025-04-16 16:16:16 +0500

    test

--- products/forms.py
+++ products/forms.py
@@ -1,7 +1,9 @@
 from django import forms
-from .models import Product
+from .models import Product, ProductImage
 
 class NewProductForm(forms.ModelForm):
+    images = forms.ImageField(widget=forms.ClearableFileInput(attrs={'multiple':True}), required=False)
+
     class Meta:
         model = Product
         fields = ('title', 'description', 'price', 'address', 'category', 'phone_number', 'tg_username')
@@ -10,4 +12,6 @@ class NewProductForm(forms.ModelForm):
             product = self.instance
             product.author = request.user   
             super().save(commit)
-            return product 
+            return product
+
+
